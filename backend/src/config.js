6109a90c68d9418bd461e604637390ef995ff1c1
Morecrown <ademma619@gmail.com>
2022-07-28 23:15:25 -0700

    update public whitelisted addresses

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xad868DeA7e8bD1D97C1b3A8d4CCA2E361049D3CA"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0xad868DeA7e8bD1D97C1b3A8d4CCA2E361049D3CA"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xad868DeA7e8bD1D97C1b3A8d4CCA2E361049D3CA","0xd083b8e262B18E2B38aE9aDe099db056825f5e3D"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
