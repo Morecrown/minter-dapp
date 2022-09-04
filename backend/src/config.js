489e1d5bdebc9caed0c1035978d2ed2e39014329
Morecrown <ademma619@gmail.com>
2022-09-04 17:35:36 +0000

    new update

--- backend/src/config.js
+++ backend/src/config.js
@@ -37,7 +37,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "eartz.netlify.app", // Replace with your website or remove this line if you do not have one.
+  external_url: "badworld.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -58,10 +58,10 @@ const MINT_PRICE = 25; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, 
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-09-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-11-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-07-18T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-09-18T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xad868DeA7e8bD1D97C1b3A8d4CCA2E361049D3CA"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
